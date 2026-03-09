7a6d1190fe546975332bc212955adc090238a3a2
kkk <gg>
2026-03-09 10:18:36 +0100

    added fixed window size and window resize ability GUI

--- Eksamen_GUI_interface.py
+++ Eksamen_GUI_interface.py
@@ -4,6 +4,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Matematik Spil")
 
+# resizeing of window for better visibility
+root.resizable(height = None ,width = None )
+root.geometry('600x400')
+
 # label is title of game in Tkinter window
 label = tk.Label(root, text="Matematik Spil")
 label.pack()
