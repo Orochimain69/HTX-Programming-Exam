372e200020cc4365344c35d9b32cd2e60113fdd1
kkk <gg>
2026-03-09 10:07:40 +0100

    add of coment and title in game window GUI

--- Eksamen_GUI_interface.py
+++ Eksamen_GUI_interface.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 
+# root is Tkinter window
 root = tk.Tk()
+root.title("Matematik Spil")
+
+# label is title of game in Tkinter window
+label = tk.Label(root, text="Matematik Spil")
+label.pack()
+
 
 
 
