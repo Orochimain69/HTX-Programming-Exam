ade28a18724c3c232e071eeb5149a55943ca211f
Emil <e>
2026-04-20 08:52:30 +0200

    put some better button names

--- Play_game.py
+++ Play_game.py
@@ -2,23 +2,54 @@ import tkinter as tk
 
 from tkinter import Label
 
+import exam_project as ex
+
+
+
+#
+#exam_project.Game_start()
+
 
 root = tk.Tk()
 
 #text = Text(root, height = 5, width = 52)
 
+while True:
+    
+    question, answer = ex.Question_maker()
 
-l = tk.Label(root, text = "2+2")
-l.config(font =("Courier", 12))
-
-
-b1 = tk.Button(root, text = "4",)
-
-b2 = tk.Button(root, text = "5",)
+    answer1 = answer + 1
+    answer2 = answer - 1
 
 
-l.pack()
-b1.pack()
-b2.pack()
 
-tk.mainloop()
+    l = tk.Label(root,
+                 text = question
+                 )
+
+    l.config(font =("Courier", 12))
+
+
+    Correctsvar = tk.Button(root,
+                   text = answer,
+                   command=lambda:ex.Answer_checking(answer,answer)
+                   )
+
+    Andetsvar = tk.Button(root,
+                   text = answer1,
+                   command=lambda:ex.Answer_checking(answer1, answer)
+                   )
+
+    Tredjesvar = tk.Button(root,
+                   text = answer2,
+                   command=lambda:ex.Answer_checking(answer2, answer)
+                   )
+
+
+
+    l.pack()
+    Correctsvar.pack()
+    Andetsvar.pack()
+    Tredjesvar.pack()
+
+    tk.mainloop()
