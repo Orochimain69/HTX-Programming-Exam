f559b53b2bd546f57dfc79b6dab5b4cf013a92de
Emil <e>
2026-04-20 08:52:11 +0200

    fixed some issues regarding functions and deleted main() function

--- exam_project.py
+++ exam_project.py
@@ -2,101 +2,95 @@ import pygame
 import random
 import math
 import time
-
+import tkinter as tk
 
 
 User_points = 0
 
-def main():
 
 
+def Question_maker():
+    
+    List_of_numbers = ['1','2','3','4','5','6','7','8','9','10']
+    
+    List_of_operators = ['+','-','*','/']
+    
+    
+    number1 = List_of_numbers[random.randint(0,9)]
+    operand = List_of_operators[random.randint(0,3)]
+    number2 = List_of_numbers[random.randint(0,9)]
+    
+    
+    
+    #Question = List_of_numbers[random.randint(0,9)] + list_of_operators[random.randint(0,3)] + List_of_numbers[random.randint(0,9)]
+    
+    Question = number1+operand+number2
+    #Question = '2/3'
+    
+    
+    answer = eval(Question)
+    
+    divider = 100
+    
+    if int(
+        (
+        (float(answer)-int(answer))*10
+        )
+           )< 5:
+        answer= math.floor(answer*divider)/divider
+    else:
+        answer = math.ceil(answer*divider)/divider
+    
+    
+    print(' ')
+    print(Question)
+    print(' ')
+    print(answer)
+    
+    return Question, answer
+    
 
-    def Game_start():
-        User_points = 0
+    
+    
+    
+    
+
+
+#print(question, answer)
+
+def Answer_checking(User_answer, correct_answer):
         
+        if float(User_answer) == correct_answer:
+            print('correct')
+        else:
+            print('WRONG!!!!')
+        #Exit = input('do you wanna exit? ')
+        #if Exit == 'y':
+            #wee = 2
+            #return 0, wee
+        
+#wee = 1    
+#while wee == 1:
+
+#question, answer = Question_maker()
+    
+#Correct_or_not, wee = Answer_checking()
         
     
-        def Question_maker():
-            
-            List_of_numbers = ['1','2','3','4','5','6','7','8','9','10']
-            
-            List_of_operators = ['+','-','*','/']
-            
-            
-            number1 = List_of_numbers[random.randint(0,9)]
-            operand = List_of_operators[random.randint(0,3)]
-            number2 = List_of_numbers[random.randint(0,9)]
-            
-            
-            
-            #Question = List_of_numbers[random.randint(0,9)] + list_of_operators[random.randint(0,3)] + List_of_numbers[random.randint(0,9)]
-            
-            Question = number1+operand+number2
-            #Question = '2/3'
-            
-            
-            answer = eval(Question)
-            
-            divider = 100
-            
-            if int(
-                (
-                (float(answer)-int(answer))*10
-                )
-                   )< 5:
-                answer= math.floor(answer*divider)/divider
-            else:
-                answer = math.ceil(answer*divider)/divider
-            
-            
-            print(' ')
-            print(Question)
-            print(' ')
-            print(answer)
-            
-            return Question, answer
-            
-
-            
-            
-            
-            
-        
-        
-        #print(question, answer)
-        def Answer_checking():
-            
-            print('Hvis svaret indholder decimaler, skal der kun angives 2.')
-            User_answer = input('Answer > ')
-            
-            if float(User_answer) == answer:
-                print('correct')
-            else:
-                print('WRONG!!!!')
-            
-            pass
-            
-        
-        question, answer = Question_maker()
-        
-        Correct_or_not = Answer_checking()
-        
-        
-        
-        
-        #User_Answer = input((question))
-
-        
-
-        #print(question[1])
-        #print(User_Answer)
     
-    Game_start()
+    
+    #User_Answer = input((question))
+
+    
+
+    #print(question[1])
+    #print(User_Answer)
+    
+#Game_start()
 
 
 
 
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "Play_game.py":
+#     main()
 
